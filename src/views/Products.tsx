29237f576d1dfc73dc5dac074f7cb5f6bf482671
notlim_dev <elmotigre6@gmail.com>
2024-10-15 11:25:10 -0500

    Definicion de Elementos y Estilos
    => se Incorporó el botón dinamico a la pagina producto-nuevo, menjado
    por el componente de Link.

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Products() {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to="productos/nuevo"
+          className="bg-indigo-600 text-white p-3 text-sm uppercase font-bold rounded-md shadow-sm hover:bg-indigo-700 transition-colors"
+        >
+          Agregar Producto
+        </Link>
+      </div>
+    </>
   )
 }
 
