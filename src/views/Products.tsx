e98f3a6084b65fd1cf3317d605a1231d54da0827
notlim_dev <elmotigre6@gmail.com>
2024-10-17 12:36:47 -0500

    Incorporacion del hook useLoaderdData.
    => el hook nos permite la consulta a nuestra de nuestro Endpoit de
    datos.

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,14 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getProducts } from "../services/ProductService";
+import Details from "../components/Details";
+import { Product } from "../types";
 
-export async function loader(){
-  const products = await getProducts() 
-  console.log(products)
- return {}
-
+export async function loader() {
+  const products = await getProducts()
+  return products
 }
 export default function Products() {
-  return (
+
+  const products = useLoaderData() as Product[]
+    return (
     <>
       <div className="flex justify-between">
         <h2 className="text-4xl font-black text-slate-500">Productos</h2>
@@ -18,6 +20,27 @@ export default function Products() {
         >
           Agregar Producto
         </Link>
+      </div>
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+           {products.map(product => (
+              <Details
+                  key={product.id}
+                  product={product}
+              />
+           ))} 
+          </tbody>
+        </table>
       </div>
     </>
   )
