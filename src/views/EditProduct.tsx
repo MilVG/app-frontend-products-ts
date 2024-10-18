b9532646a0b97926be23cb9aa73e59a98ccbc8fe
notlim_dev <elmotigre6@gmail.com>
2024-10-17 21:38:35 -0500

    Definicion e Implementacion de Actualizar Producto
    1º=> Se obtubo el producto por el id y se incorporó en el formualrio.
    2ª=> Se actualizó el producto mediante la libreria axios con el metodo
    put y tipado por valibot.

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,8 +1,22 @@
-import { Link, Form, useActionData, ActionFunctionArgs, redirect } from "react-router-dom";
+import { Link, Form, useActionData, ActionFunctionArgs, redirect, LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct } from "../services/ProductService";
+import { getProductById, updateProduct } from "../services/ProductService";
+import { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
-export async function action({ request }: ActionFunctionArgs) {
+export async function loader({ params }: LoaderFunctionArgs) {
+  if (params.id !== undefined) {
+
+    const product = await getProductById(+params.id)
+    if (!product) {
+      return redirect('/')
+      //throw new Response('', { status: 404, statusText: 'No encontrado' })
+    }
+    return product
+  }
+}
+
+export async function action({ request, params }: ActionFunctionArgs) {
 
   const data = Object.fromEntries(await request.formData())
 
@@ -14,17 +28,25 @@ export async function action({ request }: ActionFunctionArgs) {
     return error
   }
 
-  await addProduct(data)
-  return redirect('/')
+  if (params.id !== undefined) {
+    await updateProduct(data, +params.id)
+    return redirect('/')
+  }
 }
+const availabilityOptions = [
+  { name: 'Disponible', value: true },
+  { name: 'No Disponible', value: false }
+]
+
 export default function EditProduct() {
 
+  const product = useLoaderData() as Product
   const error = useActionData() as string
 
   return (
     <>
       <div className="flex justify-between">
-        <h2 className="text-4xl font-black text-slate-500">Actualizar Productos</h2>
+        <h2 className="text-4xl font-black text-slate-500">Editar Productos</h2>
         <Link
           to="/"
           className="bg-indigo-600 text-white p-3 text-sm uppercase font-bold rounded-md shadow-sm hover:bg-indigo-700 transition-colors"
@@ -35,37 +57,29 @@ export default function EditProduct() {
         className="mt-10"
         method="POST"
       >
-
+        <ProductForm
+          product={product}
+        />
         <div className="mb-4">
           <label
             className="text-gray-800"
-            htmlFor="name"
-          >Nombre Producto:</label>
-          <input
-            id="name"
-            type="text"
+            htmlFor="availability"
+          >Disponibilidad:</label>
+          <select
+            id="availability"
             className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="price"
-          >Precio:</label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map(option => (
+              <option key={option.name} value={option.value.toString()}>{option.name}</option>
+            ))}
+          </select>
         </div>
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-          value="Registrar Producto"
+          value="Guardar Cambios"
         />
       </Form>
     </>
