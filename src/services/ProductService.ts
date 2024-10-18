04e99657c8dc2fd10783106dd228432ebe0a2c2a
notlim_dev <elmotigre6@gmail.com>
2024-10-17 22:55:21 -0500

    Obtener producto y actualizar
    => se hizo la peticion del tipo put con axios y el del tipo get para la
    obtencion del producto que se requiera actulizar

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,42 +1,79 @@
-import {safeParse} from 'valibot'
-import { DraftProductSchema,ProductsSchema } from '../types';
+import { safeParse, number, string, pipe, transform, parse } from 'valibot'
+import { DraftProductSchema, Product, ProductsSchema, ProudctSchema } from '../types';
 import axios from 'axios';
+import { toBoolean } from '../utils';
 type ProductData = {
   [k: string]: FormDataEntryValue
 }
-export async function addProduct(data : ProductData){
+export async function addProduct(data: ProductData) {
   try {
-    const result = safeParse(DraftProductSchema,{
+    const result = safeParse(DraftProductSchema, {
       name: data.name,
       price: +data.price
     })
-   if(result.success){
+    if (result.success) {
       const url = `${import.meta.env.VITE_API_URL}/api/products`
-      await axios.post(url,{
+      await axios.post(url, {
         name: result.output.name,
         price: result.output.price
       })
-    }else{
+    } else {
       throw new Error('Datos no válidos')
     }
   } catch (error) {
-   console.log(error);
-    
+    console.log(error);
+
   }
 }
 
-export async function getProducts(){
+export async function getProducts() {
   try {
     const url = `${import.meta.env.VITE_API_URL}/api/products`
-    const {data} = await axios(url)
-    const result = safeParse(ProductsSchema,data.data)
+    const { data } = await axios(url)
+    const result = safeParse(ProductsSchema, data.data)
     if (result.success) {
       return result.output
-    }else{
+    } else {
       throw new Error('Hubo un error')
-    }    
+    }
   } catch (error) {
-   console.log(error);
-    
+    console.log(error);
+
   }
+}
+export async function getProductById(id: Product['id']) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+    const { data } = await axios(url)
+    const result = safeParse(ProudctSchema, data.data)
+    if (result.success) {
+      return result.output
+    } else {
+      throw new Error('Hubo un error')
+    }
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+export async function updateProduct(data: ProductData, id: Product['id']) {
+  try {
+    const NumberSchema = pipe(string(), transform(Number), number());
+    const result = safeParse(ProudctSchema, {
+      id,
+      name: data.name,
+      price: parse(NumberSchema, data.price),
+      availability: toBoolean(data.availability.toString())
+    })
+    if (result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+      await axios.put(url, result.output)
+    }
+
+  } catch (error) {
+    console.log(error);
+
+  }
+
 }
